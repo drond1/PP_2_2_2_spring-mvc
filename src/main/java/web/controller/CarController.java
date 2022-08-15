1cbb816e34c1722ec3042e7c2d2ffd0d74badd1f
drond1 <andrushhaa@mail.ru>
2022-08-15 11:01:35 +0300

    fix task 2.2.2

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -7,25 +7,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 import web.model.Car;
 import web.service.CarService;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 public class CarController {
-
+    private final CarService carService = new CarService();
     @GetMapping(value = "/cars")
     public String showCarsList(ModelMap model, @RequestParam(required = false) Integer count) {
 
-        List<Car> cars = new ArrayList<>();
-        cars.add(new Car("BMW", 1980, "Black"));
-        cars.add(new Car("AUDI", 1990, "Yellow"));
-        cars.add(new Car("FORD", 1969, "Green"));
-        cars.add(new Car("OPEL", 1979, "Blue"));
-        cars.add(new Car("VW", 2000, "White"));
         if (count == null || count >= 5) {
-            model.addAttribute("cars", CarService.getCarsList(cars, 5));
+            model.addAttribute("cars", carService.getCarsList(5));
         } else if (count > 0) {
-            model.addAttribute("cars", CarService.getCarsList(cars, count));
+            model.addAttribute("cars", carService.getCarsList(count));
         }
         return "cars";
     }
--- src/main/java/web/service/CarService.java
+++ src/main/java/web/service/CarService.java
@@ -1,13 +1,25 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CarService {
-    public static List<Car> getCarsList (List<Car> carList, int number) {
+        private final List<Car> cars;
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car("BMW", 1980, "Black"));
+        cars.add(new Car("AUDI", 1990, "Yellow"));
+        cars.add(new Car("FORD", 1969, "Green"));
+        cars.add(new Car("OPEL", 1979, "Blue"));
+        cars.add(new Car("VW", 2000, "White"));
+    }
 
-        return carList.stream().limit(number).collect(Collectors.toList());
+    public List<Car> getCarsList(int number) {
+        return cars.stream().limit(number).collect(Collectors.toList());
     }
 }
